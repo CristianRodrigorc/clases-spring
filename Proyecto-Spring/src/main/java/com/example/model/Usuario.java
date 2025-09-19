38b53a1f33e938261343696fea33d579f1a53bf5
CristianRodrigorc <cristian24121997@gmail.com>
2025-09-19 23:22:34 +0200

    cambios en el proyecto

--- Proyecto-Spring/src/main/java/com/example/model/Usuario.java
+++ Proyecto-Spring/src/main/java/com/example/model/Usuario.java
@@ -8,7 +8,7 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String name;
     private String lastname;
     private int age;
--- Proyecto-Spring/src/main/java/com/example/repository/UsuarioRepository.java
+++ Proyecto-Spring/src/main/java/com/example/repository/UsuarioRepository.java
@@ -2,7 +2,6 @@ package com.example.repository;
 
 import com.example.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
